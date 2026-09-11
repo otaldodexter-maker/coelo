dcd2e3a841e8c5e0a671b1f7a6428e267dc0a275
Adriel B Coelho <otaldodexter@gmail.com>
2026-09-11 19:46:39 -0300

    test(cardapios): remove import sem uso

--- apps/superadmin/test/app/router/meal_plan_production_routes_test.dart
+++ apps/superadmin/test/app/router/meal_plan_production_routes_test.dart
@@ -8,7 +8,6 @@ import 'package:coelo_superadmin/features/auth/domain/password_recovery.dart';
 import 'package:coelo_superadmin/features/meal_plans/data/dev/development_meal_plan_repository.dart';
 import 'package:coelo_superadmin/features/meal_plans/presentation/meal_plan_directory_page.dart';
 import 'package:coelo_superadmin/features/meal_plans/presentation/meal_plan_wizard_page.dart';
-import 'package:coelo_superadmin/features/errors/presentation/screens/superadmin_error_screen.dart';
 import 'package:coelo_tokens/coelo_tokens.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
